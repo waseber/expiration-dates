a0ca58ab700fe9c25749c961212e976ec29e0dbc
waseber <yvesgebrer@gmail.com>
2024-06-26 17:02:31 -0400

    initial commit. Adds vuetify, pinia

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-responsive class="border rounded" max-height="300">
+    <v-app>
+      <v-app-bar title="App bar"></v-app-bar>
+
+      <v-navigation-drawer>
+        <v-list>
+          <v-list-item title="Navigation drawer"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <v-container>
+          <h1>Main Content</h1>
+        </v-container>
+      </v-main>
+    </v-app>
+  </v-responsive>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
