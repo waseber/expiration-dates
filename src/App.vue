3269ace80392b14a33cf107216294ac19fced4c2
waseber <yvesgebrer@gmail.com>
2024-06-26 21:06:46 -0400

    vite fixes. New layout

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import DefaultLayout from "./layouts/DefaultLayout.vue";
+</script>
 
 <template>
-  <v-responsive class="border rounded" max-height="300">
-    <v-app>
-      <v-app-bar title="App bar"></v-app-bar>
-
-      <v-navigation-drawer>
-        <v-list>
-          <v-list-item title="Navigation drawer"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-
-      <v-main>
-        <v-container>
-          <h1>Main Content</h1>
-        </v-container>
-      </v-main>
-    </v-app>
-  </v-responsive>
+  <default-layout> Hi </default-layout>
 </template>
 
 <style scoped>
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-responsive class="border rounded" max-height="300">
+    <v-app>
+      <v-app-bar title="App bar"></v-app-bar>
+
+      <v-navigation-drawer>
+        <v-list>
+          <v-list-item title="Navigation drawer"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <slot />
+      </v-main>
+    </v-app>
+  </v-responsive>
+</template>
