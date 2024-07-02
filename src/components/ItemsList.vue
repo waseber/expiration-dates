2be8b9122353186de5dd37544ca61adae0355280
waseber <yvesgebrer@gmail.com>
2024-07-01 22:37:07 -0400

    adds conditional formatting

--- src/components/ItemsList.vue
+++ src/components/ItemsList.vue
@@ -20,16 +20,37 @@ const listData = ref([
     ],
   },
 ]);
+
+const formatDate = (date) => {
+  return new Date(date).toLocaleDateString();
+};
+
+const colorFormat = (date) => {
+  const d = new Date(date);
+  const today = Date.now();
+
+  if (d <= today) {
+    return "red-darken-4";
+  }
+
+  if (d == today) {
+    return "yellow-darken-4";
+  }
+
+  return null;
+};
 </script>
 
 <template>
   <div v-for="(g, gIdx) in listData" :key="gIdx">
-    <h2 class="text-capitalize">{{ g.location }}</h2>
+    <h2 class="text-h5 text-capitalize">{{ g.location }}</h2>
     <template v-for="(item, iIdx) in g?.items" :key="iIdx">
-      <v-list>
-        <v-list-item-title class="d-flex text-capitalize">{{
-          item.item
-        }}</v-list-item-title>
+      <v-list :bg-color="colorFormat(item.expirationDate)">
+        <v-list-item-title class="d-flex text-capitalize"
+          >{{ item.item }} <v-spacer /><span
+            v-html="formatDate(item.expirationDate)"
+          ></span
+        ></v-list-item-title>
       </v-list>
     </template>
   </div>
